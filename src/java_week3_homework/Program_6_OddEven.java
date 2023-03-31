f7a3b23231d91ac8aca549bce6601e1d059a2db5
jignaypatel <jignayp@yahoo.co.uk>
2023-03-31 19:43:20 +0100

    Java-Week3-Writing-Hw-Jigna

---
+++ src/java_week3_homework/Program_6_OddEven.java
@@ -0,0 +1,27 @@
+package java_week3_homework;
+/**
+ * Write a java program to input any number and find out if it’s odd or even
+ */
+import java.util.Scanner;
+
+public class Program_6_OddEven {
+    public static void main(String[] args) {
+        //Scanner declaration for reading input form console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number you want to check : ");
+        int number = scanner.nextInt();
+        Program_6_OddEven oddEven = new Program_6_OddEven();
+        System.out.println(number + " is " + oddEven.isItEvenOrOddNumber(number) + " number");
+        // Closing the scanner object
+        scanner.close();
+    }
+
+    // Checking the odd or even
+    public String isItEvenOrOddNumber(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+}
